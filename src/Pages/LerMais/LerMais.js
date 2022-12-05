b868e3782a5a12faf57724a4036b3cc2d03960ba
Matheus Gomes <112782424+MatheusPCRJ@users.noreply.github.com>
2022-12-05 01:03:43 -0300

    Add files via upload

---
+++ src/Pages/LerMais/LerMais.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "../../Components/Header/Header";
+
+function Lermais(){
+    return(
+        <div>
+            <Header />
+
+            <main>
+                <div className="cards">
+                    <div className="card">
+                        <header>
+                            <h2>Curso consumindo uma Api</h2>
+                        </header>
+                        <div className="line"></div>
+                        <p>Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla
+                            bla Bla bla bla Bla bla blaBla bla blaBla bla blaBla bla
+                            blabla bla Bla bla bla
+                        </p>
+
+                        
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
+}
+export default Lermais
